a7daa6b338bd249dbd798c8fa87202943898008c
Par26ival <kal.tomov@gmail.com>
2025-07-30 18:42:32 +0000

    The application is finnished. I have ommitted putting tests as they will not run whatsoever

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -3,7 +3,13 @@ class Quote < ApplicationRecord
   belongs_to :company
   validates :name, presence: true
 
+  has_many :line_items, through: :line_item_dates
+
   scope :ordered, -> { order(id: :desc) }
   
   broadcasts_to ->(quote) { [quote.company, "quotes"] }, inserts_by: :prepend
+
+  def total_price
+    line_items.sum(&:total_price)
+  end
 end
